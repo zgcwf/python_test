fb444f4259fe7c65ada1ab2e3502c17edf69c04f
zgcwf <1935083614@qq.com>
2023-06-02 17:22:28 +0800

    python

--- pyqt_test.py
+++ pyqt_test.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    # w.setGeometry(800, 400, 400, 300)  # 设置窗口位置和大小
+    # w.resize(250, 150)
+    # w.move(300, 300)
+    w.setGeometry(800, 400, 400, 300)  # 设置窗口位置和大小
     w.setWindowTitle('Hello World')
     w.show()
 
